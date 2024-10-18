1ee19384d2e7f7d307bee11884961bded192e636
marshall235 <fredie1738@gmail.com>
2024-10-18 14:49:44 +0300

    a SQL script that creates a table users following these requirements:
    With these attributes:
    id, integer, never null, auto increment and primary key
    email, string (255 characters), never null and unique
    name, string (255 characters)
    If the table already exists, your script should not fail
    Your script can be executed on any database

---
+++ 0-uniq_users.sql
@@ -0,0 +1,11 @@
+-- SQL Script that creates a table users following
+-- these requirements:  id(integer, never null, auto
+-- increment and primary key)
+-- email (string 255 characters never null and unique)
+-- name (string 255 characters)
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+)
