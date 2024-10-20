6293218322b8a9c44b0fcf78a1c4bcf0f2d77e6b
marshall235 <fredie1738@gmail.com>
2024-10-20 12:22:05 +0300

    Using the correct procedure name

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -3,7 +3,7 @@
 
 
 DELIMITER $$
-DROP PROCEDURE IF EXISTS ComputerAverageWeight;
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;;
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
 	UPDATE users set average_score = (
