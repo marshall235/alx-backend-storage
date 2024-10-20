434fa6fdd3badb7ba1fbcff1d94899851a9d37cb
marshall235 <fredie1738@gmail.com>
2024-10-20 11:57:47 +0300

    a SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
    Requirements:
    
    Procedure ComputeAverageScoreForUser is taking 1 input:
    user_id, a users.id value (you can assume user_id is linked to an existing users)

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,3 +1,8 @@
+-- An SQL Script that creates a view need_meeting
+-- that lists all students that have the score under
+-- 80 and no last_meeting or more than one month
+
+
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name FROM students WHERE score < 80
