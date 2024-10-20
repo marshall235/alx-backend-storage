aec493370590cffe38074273601bd73ac866f7f9
marshall235 <fredie1738@gmail.com>
2024-10-20 12:02:14 +0300

    Add a comma to the DATE_ADD function

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -6,4 +6,4 @@
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name FROM students WHERE score < 80
-AND (students.last_meeting IS NULL OR students.last_meeting < DATE_ADD(NOW() INTERVAL -1 MONTH));
+AND (students.last_meeting IS NULL OR students.last_meeting < DATE_ADD(NOW(), INTERVAL -1 MONTH));
