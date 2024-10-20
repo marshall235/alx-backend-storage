4c563727cd1dbe001578c076f2a8512b1f4e8a75
marshall235 <fredie1738@gmail.com>
2024-10-20 08:51:14 +0300

    a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
    Requirements:
    
    Procedure AddBonus is taking 3 inputs (in this order):
    user_id, a users.id value (you can assume user_id is linked to an existing users)
    project_name, a new or already exists projects - if no projects.name found in the table, you should create it
    score, the score value for the correction

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -5,7 +5,7 @@
 DELIMITER $$;
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-	IF NOT EXISTS(SELECT name FROM projects WHERE name =project_name) THEN
+	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
 		INSERT INTO projects (name) VALUES (project_name);
 	END IF;
 	INSERT INTO corrections (user_id, project_id, score)
