16a76e9032034b2043a838a000d535243a73a8b5
marshall235 <fredie1738@gmail.com>
2024-10-24 14:54:49 +0300

    create a get method that take a key string argument and an optional Callable argument named fn. This callable will be used to convert the data back to the desired format.
    Remember to conserve the original Redis.get behavior if the key does not exist.
    
    Also, implement 2 new methods: get_str and get_int that will automatically parametrize Cache.get with the correct conversion function.
    
    The following code should not raise:

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,7 +17,7 @@ can be a str, bytes, int or float.
 
 import redis
 import uuid
-from typing import Union
+from typing import Callable, Optional, Union
 
 
 class Cache:
@@ -36,3 +36,29 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ Convert the data back to the desired format"""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Automatically parametize Cache.get with correct
+        conversion function"""
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: int) -> Optional[int]:
+        """ Automatically parametize Cache,get with correct
+        conversion function"""
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
