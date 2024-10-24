65c59767bcb062c5543834ded42189a6ff9b8eaf
marshall235 <fredie1738@gmail.com>
2024-10-24 14:23:56 +0300

    format the code appropriately and rewrite the store function appropriately

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,7 +19,10 @@ import redis
 import uuid
 from typing import Union
 
+
 class Cache:
+    """Create class cache"""
+
     def __init__(self):
         """
         Stores the Redis client instance
@@ -28,7 +31,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float] -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Generate a random key """
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
