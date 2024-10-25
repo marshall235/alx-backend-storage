5c21f3b0fa4110ceaaedad85cd0f63f0394f07b0
marshall235 <fredie1738@gmail.com>
2024-10-25 17:33:19 +0300

    a get_page function (prototype: def get_page(url: str) -> str:). The core of the function is very simple. It uses the requests module to obtain the HTML content of a particular URL and returns it.
    Start in a new file named web.py and do not reuse the code written in exercise.py.
    
    Inside get_page track how many times a particular URL was accessed in the key count:{url} and cache the result with an expiration time of 10 seconds.

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+Implimenting a get page function that uses
+request module to obtain html content of a
+particular url and returns it
+"""
+
+
+import requests
+import redis
+from function import wraps
+
+
+r = redis.Redis()
+
+def url_access(method):
+    """
+    Decorator for get_page function
+    """
+    @wraps(method)
+    def wrapper(url):
+        """Wrapper function"""
+        key = "cached:" + 1
+        cached_value = r.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+        # Get the conntent and update cache
+
+        key_count = "count:" + url
+        html_content = method(url)
+
+        r.incr(key_count)
+        r.set(key, html_content, ex=10)
+        return html_content
+
+    return wrapper
+
+@url_access_count
+def get_page(url: str) -> str:
+    """ Obtain HTML CONTENT of a page"""
+    results = requests.get(url)
+    return results.text
+
+if __name__ = "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
